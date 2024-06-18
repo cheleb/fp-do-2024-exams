aee1e7377cbfbca47ad93b724500983917c1c4cc
Charley <charley.geoffroy@protonmail.com>
2024-06-18 12:39:03 +0200

    docs: add comments to existing code
    Signed-off-by: Charley <charley.geoffroy@protonmail.com>

--- src/main/scala/dotapir/server/HttpApi.scala
+++ src/main/scala/dotapir/server/HttpApi.scala
@@ -6,11 +6,13 @@ import sttp.tapir.server.ServerEndpoint
 import controllers.*
 
 object HttpApi {
+  // Prend une liste de controllers et retourne une liste de ServerEndpoints
   private def gatherRoutes(
       controllers: List[BaseController]
   ): List[ServerEndpoint[Any, Task]] =
     controllers.flatMap(_.routes)
 
+  // Crée une liste de controllers
   private def makeControllers = for {
     healthController <- HealthController.makeZIO
     personController <- PersonController.makeZIO
