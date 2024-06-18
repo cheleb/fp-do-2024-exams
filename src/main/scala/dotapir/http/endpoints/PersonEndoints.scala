60df2bc3e4affb8f542912d5db0eb0f4fa24dba6
MurielParaire <72733662+MurielParaire@users.noreply.github.com>
2024-06-18 12:22:35 +0200

    fix: list endpoint add good tag

--- src/main/scala/dotapir/http/endpoints/PersonEndoints.scala
+++ src/main/scala/dotapir/http/endpoints/PersonEndoints.scala
@@ -13,25 +13,28 @@ object PersonEndpoints extends BaseEndpoint:
   // create endpoint to create a user from a person
   val createEndpoint: Endpoint[Unit, Person, Throwable, User, Any] =
     baseEndpoint
-      // endpoint path
+      // tag - to regroup with swagger
       .tag("person")
       .name("person")
       .post
-      // name of the argument
+      // endpoint path
       .in("person")
-      // type of argument
+      // expected body type
       .in(
         jsonBody[Person]
           .description("Person to create")
+          // example for body / placeholder / test
           .example(Person("John", 30, "jonh@dev.com"))
       )
+      // return type
       .out(jsonBody[User])
       .description("Create person")
       
   val listEndpoint: Endpoint[Unit, Unit, Throwable, List[User], Any] =
     baseEndpoint
-      .tag("persons")
+      .tag("person")
       .name("persons")
       .get
+      .in("persons")
       .out(jsonBody[List[User]])
       .description("Get all persons")
