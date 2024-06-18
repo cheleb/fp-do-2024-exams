6706786ef47bd6df4caebd592e6e76d4cd17cba4
MurielParaire <72733662+MurielParaire@users.noreply.github.com>
2024-06-18 11:55:13 +0200

    feat: add list persons

--- src/main/scala/dotapir/http/endpoints/PersonEndoints.scala
+++ src/main/scala/dotapir/http/endpoints/PersonEndoints.scala
@@ -10,12 +10,16 @@ import dotapir.model.*
 
 object PersonEndpoints extends BaseEndpoint:
 
+  // create endpoint to create a user from a person
   val createEndpoint: Endpoint[Unit, Person, Throwable, User, Any] =
     baseEndpoint
+      // endpoint path
       .tag("person")
       .name("person")
       .post
+      // name of the argument
       .in("person")
+      // type of argument
       .in(
         jsonBody[Person]
           .description("Person to create")
@@ -23,3 +27,11 @@ object PersonEndpoints extends BaseEndpoint:
       )
       .out(jsonBody[User])
       .description("Create person")
+      
+  val listEndpoint: Endpoint[Unit, Unit, Throwable, List[User], Any] =
+    baseEndpoint
+      .tag("persons")
+      .name("persons")
+      .get
+      .out(jsonBody[List[User]])
+      .description("Get all persons")
--- src/main/scala/dotapir/server/controllers/PersonController.scala
+++ src/main/scala/dotapir/server/controllers/PersonController.scala
@@ -12,15 +12,23 @@ import dotapir.repository.UserRepository
 
 class PersonController(userRepository: UserRepository) extends BaseController {
 
+  // create a new user from a person
   val create: ServerEndpoint[Any, Task] = PersonEndpoints.createEndpoint
+    // case person => if a person was given
     .zServerLogic { case (person) =>
       userRepository.create(
+        // -1 as id since it is necessary for the object User but no need to put a real value because the id is generated automatically in the db
         User(-1, person.name, person.age, person.email, ZonedDateTime.now())
       )
     }
+  
+    val get: ServerEndpoint[Any, Task] = PersonEndpoints.listEndpoint
+    .zServerLogic { case () =>
+      userRepository.get()
+    }
 
   val routes: List[ServerEndpoint[Any, Task]] =
-    List(create)
+    List(create, get)
 }
 
 object PersonController {
